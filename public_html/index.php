b4a0a6f0ce747b93267668a98c80a5ac39f60c46
rgarro <rgarro@gmail.com>
2022-08-26 16:28:47 -0500

    la revolucion no se va ver por television   ....

--- public_html/index.php
+++ public_html/index.php
@@ -1,2 +1,31 @@
 <?php
-echo "chunky Bacon! chunky Bacon! where is my pickup?";
+//echo "chunky Bacon! chunky Bacon! where is my pickup?";
+/**
+ *           __
+ *.-.__      \ .-.  ___  __
+ *|_|  '--.-.-(   \/\;;\_\.-._______.-.
+ *(-)___     \ \ .-\ \;;\(   \       \ \
+ * Y    '---._\_((Q)) \;;\\ .-\     __(_)
+ * I           __'-' / .--.((Q))---'    \,
+ * I     ___.-:    \|  |   \'-'_          \
+ * A  .-'      \ .-.\   \   \ \ '--.__     '\
+ * |  |____.----((Q))\   \__|--\_      \     '
+ *    ( )        '-'  \_  :  \-' '--.___\
+ *     Y                \  \  \       \(_)
+ *     I                 \  \  \         \,
+ *     I                  \  \  \          \
+ *     A                   \  \  \          '\
+ *     |              snd   \  \__|           '
+ *                           \_:.  \
+ *                             \ \  \
+ *                              \ \  \
+ *                               \_\_|
+ * 
+ * :: CAKEPHP TEQUILA TRES LECHES WITH BLUE BERRY :::
+ * While hidding out in the Endor Moons I had a SlingShot Turbine comming from Tatooine
+ * Master Yoda I need a F150 Ford PickUp, Foil Fins from Alderan
+ * SkyWalker 5'10 Bailing out from the Black Star
+ * 
+ * 
+ * @author Rolando<rolando@emptyart.xyz>
+ */
