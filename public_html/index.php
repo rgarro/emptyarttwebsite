353fd9d12b0d14d7e607962c0ed0f0dd2adb29ff
rgarro <rgarro@gmail.com>
2022-08-30 04:43:47 -0500

    vote for cara marion

--- public_html/index.php
+++ public_html/index.php
@@ -56,7 +56,7 @@
         <!-- Masthead-->
         <header class="masthead bg-primary text-white text-center">
             <div class="container d-flex align-items-center flex-column">
-                xxxxx
+                index
             </div>
         </header>
         
