a75fd08cc3c3a8224f2897fb769a2b3f95523820
rgarro <rgarro@gmail.com>
2022-08-31 04:25:12 -0500

    beyond the evil and the deep blue sea

---
+++ includes/navigation.php
@@ -0,0 +1,5 @@
+<a class="navbar-brand" href="/index.php">EmptyArt <small>web programming</small></a>
+<button class="navbar-toggler text-uppercase font-weight-bold bg-primary text-white rounded" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+    Menu
+<i class="fas fa-bars"></i>
+</button>
