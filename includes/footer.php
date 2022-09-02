119c20b32dc29db6e9190a865353a6fb5533649e
rgarro <rgarro@gmail.com>
2022-09-02 06:08:32 -0500

    beggin alms in snowed Denver while doing a vajravarahi retreat II

--- includes/footer.php
+++ includes/footer.php
@@ -13,11 +13,13 @@
                     <!-- Footer Social Icons-->
                     <div class="col-lg-4 mb-5 mb-lg-0">
                         <h4 class="text-uppercase mb-4">Around the Web</h4>
-                        <a class="btn btn-outline-light btn-social mx-1" href="https://github.com/rgarro"><i class="fab fa-fw fa-github"></i></a>
-                        <a class="btn btn-outline-light btn-social mx-1" href="https://twitter.com/Kan_Garro"><i class="fab fa-fw fa-twitter"></i></a>
-                        <a class="btn btn-outline-light btn-social mx-1" href="#!"><i class="fab fa-fw fa-Sketchfab"></i></a>
-                        <a class="btn btn-outline-light btn-social mx-1" href="https://www.behance.net/rolandogarro"><i class="fab fa-fw fa-behance"></i></a>
-                        <a class="btn btn-outline-light btn-social mx-1" href="https://www.npmjs.com/~rgarro"><i class="fab fa-fw fa-npm"></i></a>
+                        <a class="btn btn-outline-light btn-social mx-1" target="_blank" href="https://github.com/rgarro"><i class="fab fa-fw fa-github"></i></a>
+                        <a class="btn btn-outline-light btn-social mx-1" target="_blank" href="https://twitter.com/Kan_Garro"><i class="fab fa-fw fa-twitter"></i></a>
+                        <a class="btn btn-outline-light btn-social mx-1" target="_blank" href="https://sketchfab.com/RolandoGarroMena"><i class="fab fa-fw fa-sketch"></i></a>
+                        <a class="btn btn-outline-light btn-social mx-1" target="_blank" href="https://www.behance.net/rolandogarro"><i class="fab fa-fw fa-behance"></i></a>
+                        <a class="btn btn-outline-light btn-social mx-1" target="_blank" href="https://www.npmjs.com/~rgarro"><i class="fab fa-fw fa-npm"></i></a>
+                        <a class="btn btn-outline-light btn-social mx-1" target="_blank" href="https://play.unity.com/u/RolandoGarroMena"><i class="fab fa-fw fa-unity"></i></a>
+                        <a class="btn btn-outline-light btn-social mx-1" target="_blank" href="https://soundcloud.com/dorje-trolo"><i class="fab fa-fw fa-soundcloud"></i></a>
                     </div>
                     <!-- Footer About Text-->
                     <div class="col-lg-4">
--- includes/head.php
+++ includes/head.php
@@ -2,7 +2,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta name="description" content="" />
         <meta name="author" content="" />
-        <title>Freelancer - ::: CakePHP Tequila Tres Leches :::</title>
+        <title>Freelancer - ::: Custom Web Programing WEBGL PHP LINUX WEBDESIGN :::</title>
         <!-- Favicon-->
         <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
         <!-- Font Awesome icons (free version)-->
