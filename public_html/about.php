5bb634d6cf0e7017c84cdd93a466909a9a12e944
rgarro <rgarro@gmail.com>
2022-09-01 21:02:32 -0500

    beggin alms in snowed Denver while doing a vajravarahi retreat

--- public_html/about.php
+++ public_html/about.php
@@ -27,20 +27,41 @@
  * ABOUT
  * 
  * Yo era un aprendiz de Miguel Salguero
- * para ser agente de la dis, hay que descabezar un insurrecto de perez zeledon
- * encontrar una piedra de roma en cartago y saber hacer granadas con agua y hielo seco
+ * para ser agente de la DIS, hay que descabezar un insurrecto de Perez Zeledon
+ * encontrar una piedra de Roma en Cartago y saber hacer granadas con agua y hielo seco
  * el puente de Breda cayo en coronado, los cangrejeros de la aduana resistieron
- * las doncellas de curridabath se suicidaron, piedreros que cobran peajes en las cuevas del virilla
- * Dominicanos mercenarios de jhonny araya
+ * las doncellas de Curridabath se suicidaron, piedreros que cobran peajes en las cuevas del virilla
+ * Dominicanos mercenarios de Jhonny Araya
  * Lijamos el Bluebird de Miguel Angel Rodriguez mientras firmo 7 sentencias de muerte
- * formalina del calderon guardia, ecos fantasmas del sanjuan de dios, desmilitarizados del chapui
- * Tierra de medellin en el cementerio general, el verdadero jhonny araya murio en tripoli en 1987
+ * formalina del Calderon Guardia, ecos fantasmas del SanJuan de Dios, desmilitarizados del Chapui
+ * Tierra de medellin en el cementerio general, el verdadero Jhonny araya murio en tripoli en 1987
  * prostibulos de 5 esquinas , mafias de autobuses, zanates en la ciudad capital ...
- * Miguel Salguero Mato a Chemise, se la cortaron Hugo Espadafora, 
- * viviana gallardo traiciono a unos playos de la guardia civil
+ * Cannabis de la Leon 13, jamaiquina, cajeta , punto cripi y colombiana
+ * Miguel Salguero Mato a Chemise, se la cortaron a Hugo Espadafora, 
+ * Viviana gallardo traiciono a unos playos de la guardia civil
+ * Jotos de la Fuerza Publica en sus toyotas turbo y uniformes cocidos por moros en la franja de gaza ...
  * Putas del poder judicial , amigos de la ANEP, ruth angulo, camaradas de canal 13, amigos de diario extra
+ * Putas de los call center y parques industriales, Pintas de la Barra de Tibas, mafiosos de Hatillo
+ * Frankling Chang se compro un birra en la calle de la amargura y se la tomo en el lado oscuro de la luna
+ * se fumo un puro de jamaiquina y dijo que era el sicopata de tarrazu un monaguillo de las Delicias de Alajuela
+ * Quinceaneras de los anos setenta fomentaban el CBD entre sus primas, broncas del estanco y
+ * becados de circunciciones bancarias, traidores de Orlich y Carazo.
+ * Corazon Aquino trajo putas de Bututuan y rescato a 200 playos que nos dejo el Instituto Tecnologico 
+ * Tortilleras de la UCR accionistas del Mall Sanpedro, Caribenos y Tuasa
+ * PADRE MINOR Ruega por nosotros, bendicenos en tu gloria, que saprissa meta un gol.
+ * Rastas de Puerto Viejo, Tapon y Geova el del Guato leyendo la teja con las Putas de Jaco
+ * mariguanos de sportbook, locos de la universidad veritas ,combatientes del frente sandinista, rapidos heredianos
+ * Playos Bailarines del Conservatorio Castella, Violadores del SanFrancis, Arrancatroncas de San Miguel de Heredia
+ * peliando contra la Barra de Tibas en LeBarons en los 80 antes del tunel del Zurqui
+ * Barra Tecno de la Discoteq Kilates, Barra Punk de Heredia, Surfos del Bar Oceano, Kranio Metal de la fosforera
+ * Bailes de Salon Comunal con disco movil, radio am , canal 2, banco anglo, cine libano y barra skate de plaza viques
+ * Rodrigo Fournier murio en 1969, Carmen Lira nunca existio, La segua , el cadejos y la carreta sin bueyes 
+ * Visiones directas de Tomas Guardia, Tinoco , centroamerica de morazan, balas magicas de timbuctu , de gao, impactando el museo nacional
+ * Chilindrina, Chavo del 8, kiko y don ramon ...
  * Mis Ancestros fumaban Cachimba, daban perico a los penitentes, mataban los vecinos de los asesinos
- * Adios Amigos, me he ido a morir a los volcanes nevados de mexico .....
+ * Adios Amigos!!, me he ido a morir a los volcanes nevados de Mexico .....
+ * 
+ * 
  * 
  * @author Rolando<rolando@emptyart.xyz>
  */
