320c9f9d30e3eb41e55b3dafd6b43804d5ec0930
rgarro <rgarro@gmail.com>
2022-09-11 17:32:07 -0500

    ahi ahi esta viendo pasar el tiempo , la puerta de alcala ..

--- public_html/contact.php
+++ public_html/contact.php
@@ -70,7 +70,28 @@
     <a href="#" class="card-link"><a class="btn btn-outline-light" target="_blank" href=" https://wa.me/+18504610264"><i class="fab fa-fw fa-whatsapp"></i> +18504610264 </a>
   </div>
 </div>
+</br></br>  
+            <div class="card bg-dark" style="width: 18rem;">
+            <div class="card-body">
+            <form>
+            <div class="mb-3">
+    <label for="exampleInputName" class="form-label">Name</label>
+    <input type="text" class="form-control" id="exampleInputName">
+  </div>              
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Email address</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+  </div>
+  
+  <div class="mb-3 form-check">
+    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
             </div>
+        </div>
         </header>
         
         <!-- Footer-->
