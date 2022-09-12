108525d94300db0410f99fe67e22cb50421bac8d
rgarro <rgarro@gmail.com>
2022-09-12 15:07:01 -0500

    ahi ahi esta viendo pasar el tiempo , la puerta de alcala ..

--- public_html/contact.php
+++ public_html/contact.php
@@ -45,6 +45,7 @@
         <?php
         include_once("../includes/head.php");
         ?>
+        <script src="/js/jquery.min.js"></script>
     </head>
     <body id="page-top">
         <!-- Navigation-->
@@ -73,21 +74,21 @@
 </br></br>  
             <div class="card bg-dark" style="width: 18rem;">
             <div class="card-body">
-            <form>
+            <form id="contactForm">
             <div class="mb-3">
     <label for="exampleInputName" class="form-label">Name</label>
-    <input type="text" class="form-control" id="exampleInputName">
+    <input type="text" required="required" class="form-control" id="exampleInputName">
   </div>              
   <div class="mb-3">
     <label for="exampleInputEmail1" class="form-label">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    <input type="email" class="form-control" required="required" id="exampleInputEmail1" aria-describedby="emailHelp">
     <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
   </div>
+  <div class="mb-3">
+    <label for="exampleInputMsg" class="form-label">Message</label>
+    <input type="text" class="form-control" id="exampleInputMsg" required="required">
+  </div>       
   
-  <div class="mb-3 form-check">
-    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-    <label class="form-check-label" for="exampleCheck1">Check me out</label>
-  </div>
   <button type="submit" class="btn btn-primary">Submit</button>
 </form>
             </div>
@@ -112,5 +113,13 @@
         <!-- * * Activate your form at https://startbootstrap.com/solution/contact-forms * *-->
         <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
         <script src="https://cdn.startbootstrap.com/sb-forms-latest.js"></script>
+        <script>
+    $(document).ready(function(){
+        $("#contactForm").submit(function( event ) {
+            alert( "Handler for .submit() called." );
+            event.preventDefault();
+        });
+    });
+    </script>
     </body>
 </html>
